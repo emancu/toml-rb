758992196add8cdf5bca8b6f12598b15f64e2dd2
eMancu <emiliano.mancuso@gmail.com>
2013-02-24 14:07:49 -0300

    TOML grammar and tests for each parser rule

---
+++ test/toml_test.rb
@@ -0,0 +1,55 @@
+require "test/unit"
+require 'citrus'
+Citrus.load '/Users/emancu/Sites/toml_parser-ruby/lib/toml'
+
+class TomlTest < Test::Unit::TestCase
+  def test_comment
+    match = Toml.parse('# This is a comment', :root => :comment)
+    assert(match)
+    assert_equal('# This is a comment', match)
+  end
+
+  def test_keygroup
+    match = Toml.parse('[akey]', :root => :keygroup)
+    assert(match)
+    assert_equal('[akey]', match)
+    assert_equal('akey', match.value)
+  end
+
+  def test_keyvalue_string
+    match = Toml.parse('title = "TOML Example"', :root => :keyvalue)
+    assert(match)
+    assert_equal('title = "TOML Example"', match)
+    assert_equal('title', match.key)
+    assert_equal('TOML Example', match.val)
+    assert_equal({"title" => "TOML Example"}, match.value)
+  end
+
+  def test_keyvalue_bool
+    match = Toml.parse('enabled = true', :root => :keyvalue)
+    assert(match)
+    assert_equal('enabled = true', match)
+    assert_equal('enabled', match.key)
+    assert_equal(true, match.val)
+    assert_equal({"enabled" => true}, match.value)
+  end
+
+  def test_keyvalue_integer
+    match = Toml.parse('age = 26', :root => :keyvalue)
+    assert(match)
+    assert_equal('age = 26', match)
+    assert_equal('age', match.key)
+    assert_equal(26, match.val)
+    assert_equal({"age" => 26}, match.value)
+  end
+
+  def test_keyvalue_float
+    match = Toml.parse('height = 1.69', :root => :keyvalue)
+    assert(match)
+    assert_equal('height = 1.69', match)
+    assert_equal('height', match.key)
+    assert_equal(1.69, match.val)
+    assert_equal({"height" => 1.69}, match.value)
+  end
+end
+
