041c1ebfab4d7ab81ecbe66f32f36f55530ac790
eMancu <emiliano.mancuso@gmail.com>
2013-02-27 18:10:25 -0300

    Add rule to parse a whole file.

---
+++ test/parser_test.rb
@@ -0,0 +1,47 @@
+require_relative "helper"
+
+class ParserTest < Test::Unit::TestCase
+
+  def test_text
+    file =<<-TOML
+        # This is a comment
+        # and other comment
+
+        title = "TOML Example"
+
+        # More comments
+        # In other lines
+
+        [owners] # A List of owners
+
+          [owners.emancu]
+            name = "E. Mancuso"
+            age = 26
+
+          [owners.tonchis]
+            name = "L. Tolchinsky"
+            age = 24
+
+        [version]
+        zape = true
+    TOML
+
+    hash = {
+      "title" => "TOML Example",
+      "owners" => {
+        "emancu" => {
+          "name" => "E. Mancuso",
+          "age" => 26
+        },
+        "tonchis" => {
+          "name" => "L. Tolchinsky",
+          "age" => 24
+        }
+      }
+    }
+
+    match = Toml.parse file
+    assert_equal hash, match.value
+  end
+
+end
