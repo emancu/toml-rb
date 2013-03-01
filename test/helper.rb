d5d179e897ce33ce81cc4c72c0c6ae686c087a4b
tonchis <lucas.tolchinsky@gmail.com>
2013-03-01 17:43:47 -0300

    Adds init.rb to load files.

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,2 @@
 require "test/unit"
-require 'citrus'
-require_relative '../lib/toml/keyvalue'
-require_relative '../lib/toml/keygroup'
-require_relative '../lib/toml/parser'
-
-Citrus.load(File.dirname(__FILE__) + "/../lib/toml/grammars/primitive.citrus")
-Citrus.load(File.dirname(__FILE__) + "/../lib/toml/grammars/array.citrus")
-Citrus.load(File.dirname(__FILE__) + "/../lib/toml/grammars/document.citrus")
-
+require_relative "../init"
