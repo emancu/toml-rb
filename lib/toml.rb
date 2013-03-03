7d45d49ffa434ed3d9746290a55a1fd96c516570
tonchis <lucas.tolchinsky@gmail.com>
2013-03-03 18:23:54 -0300

    Removes TOML.load in favor of TOML.load_file and TOML.parse.

--- lib/toml.rb
+++ lib/toml.rb
@@ -3,15 +3,11 @@ require_relative "../init"
 module TOML
   VERSION = '0.3'
 
-  def self.load(content, symbolize_keys = false)
+  def self.parse(content, symbolize_keys = false)
     Parser.new(content, symbolize_keys).hash
   end
 
-  def self.parse(path, symbolize_keys = false)
-    TOML.load(File.read(path), symbolize_keys)
-  end
-
   def self.load_file(path, symbolize_keys = false)
-    TOML.parse(path, symbolize_keys)
+    TOML.parse(File.read(path), symbolize_keys)
   end
 end
