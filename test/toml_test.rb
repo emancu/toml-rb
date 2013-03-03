ee518469f28bbe51b1bebc7171d6d6bb581191eb
tonchis <lucas.tolchinsky@gmail.com>
2013-03-03 18:36:12 -0300

    Uses 1.9 hash notation.

--- test/toml_test.rb
+++ test/toml_test.rb
@@ -70,36 +70,36 @@ class TomlTest < Test::Unit::TestCase
     parsed = TOML::Parser.new(File.read(path), true)
 
     hash = {
-      :title => "TOML Example",
+      title: "TOML Example",
 
-      :owner => {
-        :name => "Tom Preston-Werner",
-        :organization => "GitHub",
-        :bio => "GitHub Cofounder & CEO\nLikes tater tots and beer.",
-        :dob => Time.utc(1979,05,27,07,32,00)
+      owner: {
+        name: "Tom Preston-Werner",
+        organization: "GitHub",
+        bio: "GitHub Cofounder & CEO\nLikes tater tots and beer.",
+        dob: Time.utc(1979,05,27,07,32,00)
       },
 
-      :database => {
-        :server => "192.168.1.1",
-        :ports => [ 8001, 8001, 8002 ],
-        :connection_max => 5000,
-        :enabled => true
+      database: {
+        server: "192.168.1.1",
+        ports: [ 8001, 8001, 8002 ],
+        connection_max: 5000,
+        enabled: true
       },
 
-      :servers => {
-        :alpha => {
-          :ip => "10.0.0.1",
-          :dc => "eqdc10"
+      servers: {
+        alpha: {
+          ip: "10.0.0.1",
+          dc: "eqdc10"
         },
-        :beta => {
-          :ip => "10.0.0.2",
-          :dc => "eqdc10"
+        beta: {
+          ip: "10.0.0.2",
+          dc: "eqdc10"
         }
       },
 
-      :clients => {
-        :data => [["gamma", "delta"], [1, 2]],
-        :hosts => ["alpha", "omega"]
+      clients: {
+        data: [["gamma", "delta"], [1, 2]],
+        hosts: ["alpha", "omega"]
       }
     }
 
