45e2dfe52821113fcc34c0ba340c739899da1491
tonchis <lucas.tolchinsky@gmail.com>
2013-04-13 13:28:47 -0300

    Less LOCs FTW! Because why not?

--- lib/toml/parser.rb
+++ lib/toml/parser.rb
@@ -8,9 +8,7 @@ module TOML
       @symbolize_keys = options[:symbolize_keys]
 
       parsed = Document.parse(content)
-      parsed.matches.map(&:value).compact.each do |match|
-        match.commit_into_hash(self)
-      end
+      parsed.matches.map(&:value).compact.each {|match| match.commit_into_hash(self)}
     end
 
     def commit_keygroup_into_hash(keygroup)
