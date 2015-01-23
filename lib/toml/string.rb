9765fff81d9aea1c58a2f3c7bdd5ed3436e4dba7
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-01-23 13:43:16 -0300

    Refactor TomlString method

--- lib/toml/string.rb
+++ lib/toml/string.rb
@@ -1,27 +1,18 @@
 # Used in primitive.citrus
 module TomlString
   def value
-    aux = first.value
-    s = 0
-    o = []
-    while s < aux.length
-      if aux[s] == '\\'
-        s += 1
-        case aux[s]
-        when 't' then o << "\t"
-        when 'n' then o << "\n"
-        when '\\' then o << '\\'
-        when '"' then o << '"'
-        when 'r' then o << "\r"
-        when '0' then o << "\0"
-        else
-          o << '\\' << aux[s]
-        end
-      else
-        o << aux[s]
-      end
-      s += 1
-    end
-    o[1...-1].join
+    aux = TomlString.transform_escaped_chars first.value
+
+    aux[1...-1]
+  end
+
+  def self.transform_escaped_chars(str)
+    str
+      .gsub(/\\0/, "\0")
+      .gsub(/\\t/, "\t")
+      .gsub(/\\n/, "\n")
+      .gsub(/\\\"/, '"')
+      .gsub(/\\r/, "\r")
+      .gsub(/\\\\/, '\\')
   end
 end
