48cb5f2ce139f97a9007cadaa7e8301e7c876b47
emancu <emiliano.mancuso@gmail.com>
2015-04-17 21:49:18 -0300

    Create a specific class to handle InlineTables

--- lib/toml/inline_table.rb
+++ lib/toml/inline_table.rb
@@ -1,8 +1,63 @@
+module TOML
+  class InlineTable
+    attr_reader :symbolize_keys
+
+    def initialize(keyvalue_pairs)
+      @pairs = keyvalue_pairs
+      @symbolize_keys = false
+    end
+
+    def value(symbolize_keys = false)
+      if @symbolize_keys = symbolize_keys
+        tuple = ->(kv) { [kv.key.to_sym, visit_value(kv.value)] }
+      else
+        tuple = ->(kv) { [kv.key, visit_value(kv.value)] }
+      end
+
+      Hash[@pairs.map &tuple]
+    end
+
+    def visit_inline_table(inline_table)
+      result = {}
+
+      inline_table.value(@symbolize_keys).each do |k, v|
+        result[key k] = visit_value v
+      end
+
+      result
+    end
+
+    def accept_visitor(keyvalue)
+      keyvalue.visit_inline_table self
+    end
+
+    private
+
+    def visit_value(a_value)
+      return a_value unless a_value.respond_to? :accept_visitor
+
+      a_value.accept_visitor self
+    end
+
+    def key(a_key)
+      symbolize_keys ? a_key.to_sym : a_key
+    end
+  end
+
+  class InlineTableArray
+    def initialize(inline_tables)
+      @inline_tables = inline_tables
+    end
+
+    def value(symbolize_keys = false)
+      @inline_tables.map { |it| it.value(symbolize_keys) }
+    end
+  end
+end
+
 module InlineTable
   def value
-    keyvalue_pairs = captures[:keyvalue].map(&:value)
-
-    Hash[keyvalue_pairs.map { |kv| [kv.key, kv.value] }]
+    TOML::InlineTable.new captures[:keyvalue].map(&:value)
   end
 end
 
@@ -10,6 +65,6 @@ module InlineTableArray
   def value
     tables = captures[:hash_array].map { |x| x.captures[:inline_table] }
 
-    tables.flatten.map(&:value)
+    TOML::InlineTableArray.new tables.flatten.map(&:value)
   end
 end
