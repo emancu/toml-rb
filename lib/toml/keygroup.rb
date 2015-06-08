2d0bbcb57716518b45bea59d49203302fddd8186
timcolonel <timothee.guerin@outlook.com>
2015-06-08 15:49:07 +0100

    Rubocop style

--- lib/toml/keygroup.rb
+++ lib/toml/keygroup.rb
@@ -5,19 +5,22 @@ module TOML
     end
 
     def navigate_keys(hash, visited_keys, symbolize_keys = false)
-      fail ValueOverwriteError.new(full_key) if visited_keys.include?(full_key)
-      visited_keys << full_key
+      ensure_key_not_defined(visited_keys)
       @nested_keys.each do |key|
         key = symbolize_keys ? key.to_sym : key
-        # do not allow to define more than once just the last key
         hash[key] = {} unless hash.key?(key)
         element = hash[key]
         hash = element.is_a?(Array) ? element.last : element
         # check that key has not been defined before as a scalar value
         fail ValueOverwriteError.new(key) unless hash.is_a?(Hash)
       end
-
       hash
+    end
+
+    # Fail if the key was already defined with a ValueOverwriteError
+    def ensure_key_not_defined(visited_keys)
+      fail ValueOverwriteError.new(full_key) if visited_keys.include?(full_key)
+      visited_keys << full_key
     end
 
     def accept_visitor(parser)
--- test/grammar_test.rb
+++ test/grammar_test.rb
@@ -30,7 +30,6 @@ class GrammarTest < Minitest::Test
     assert_equal(%w(owner.emancu),
                  match.value.instance_variable_get('@nested_keys'))
 
-
     match = TOML::Document.parse('["first key"."second key"]', root: :keygroup)
     assert_equal(['first key', 'second key'],
                  match.value.instance_variable_get('@nested_keys'))
