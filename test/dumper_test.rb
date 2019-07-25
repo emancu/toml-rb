8b79d29afab4fbec5ce008ba0e485651f418a1ef
Jānis Vanags <janonkulis@gmail.com>
2019-07-25 16:56:02 +0300

    remove extra spacing

--- test/dumper_test.rb
+++ test/dumper_test.rb
@@ -95,7 +95,7 @@ class DumperTest < Minitest::Test
   def test_dump_interpolation_curly
     hash = { "key" => 'includes #{variable}' }
     dumped = TomlRB.dump(hash)
-    assert_equal  'key = "includes #{variable}"' + "\n", dumped
+    assert_equal 'key = "includes #{variable}"' + "\n", dumped
   end
 
   def test_dump_interpolation_at
